a3e9950ed186d2c56c0bf83b8fe0857a6aa17ccf
Lona Matshingana <71089848+Lona-web558@users.noreply.github.com>
2022-08-29 09:07:09 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+#machine learning
+
+import pandas as pd
+iris_data = pd.read_csv("iris.csv")
+print("\nKeys of Iris Dataset: ")
+print(iris_data.keys())
+print("\nNumber of rows and columns: ")
+print(iris_data.shape)
